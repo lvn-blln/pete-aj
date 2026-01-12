ca892fa87bd09a565139c7dfd1b8dc3e746c5347
lvn-blln <blowaballoon@gmail.com>
2026-01-12 05:06:51 +0100

    php mailer

--- api/index.php
+++ api/index.php
@@ -1,5 +1,11 @@
 <?php
-require(__DIR__ . '/includes/PHPMailer/class.phpmailer.php');
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require __DIR__ . '/PHPMailer/src/Exception.php';
+require __DIR__ . '/PHPMailer/src/PHPMailer.php';
+// ... other requires if needed
+
 
 
 ?>
