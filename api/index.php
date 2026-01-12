ffe95684986edc6567d50411a44100af2abe3ab2
lvn-blln <blowaballoon@gmail.com>
2026-01-12 05:10:21 +0100

    template

--- api/index.php
+++ api/index.php
@@ -490,155 +490,56 @@ require __DIR__ . '/PHPMailer/src/PHPMailer.php';
                                         <form class="js-form" novalidate="novalidate" method="post">
                                            <?php
                                            if(isset($_REQUEST["send"])){
-                                        $email=$_REQUEST["email"];
+                                        
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    http_response_code(403);
+    exit("Forbidden");
+}
 
-                                            // send otp to mail
+$name    = trim($_POST["name"] ?? "");
+$email   = trim($_POST["email"] ?? "");
+$message = trim($_POST["message"] ?? "");
+$subject = trim($_POST["subject"] ?? "");
 
-                                            $register="<style>
-                                                        html,
-                                                        body {
-                                                            margin: 0 auto !important;
-                                                            padding: 0 !important;
-                                                            height: 100% !important;
-                                                            width: 100% !important;
-                                                            font-family: 'myFontII', sans-serif;
-                                                            font-size: 14px !important;
-                                                            margin-bottom: 10px !important;
-                                                            line-height: 24px;
-                                                            color: #8094ae;
-                                                            font-weight: 400 !important;
-                                                        }
-                                                        * {
-                                                            -ms-text-size-adjust: 100%;
-                                                            -webkit-text-size-adjust: 100%;
-                                                            margin: 0;
-                                                            padding: 0;
-                                                        }
-                                                        table,
-                                                        td {
-                                                            mso-table-lspace: 0pt !important;
-                                                            mso-table-rspace: 0pt !important;
-                                                        }
-                                                        table {
-                                                            border-spacing: 0 !important;
-                                                            border-collapse: collapse !important;
-                                                            table-layout: fixed !important;
-                                                            margin: 0 auto !important;
-                                                        }
-                                                        table table table {
-                                                            table-layout: auto;
-                                                        }
-                                                        a {
-                                                            text-decoration: none;
-                                                        }
-                                                        img {
-                                                            -ms-interpolation-mode:bicubic;
-                                                        }
-                                                        </style>
+if ($name === "" || $email === "" || $message === "" ) {
+    exit("All fields are required.");
+}
 
-                                                        <center style='width: 100%; background-color: #f5f6fa;'>
-                                                        <table width='100%' border='0' cellpadding='0' cellspacing='0' bgcolor='#f0f0f0'>
-                                                            <tr>
-                                                            <td style='padding: 40px 0;'>
-                                                                    <table style='width:100%;max-width:620px;margin:0 auto;'>
-                                                                        <tbody>
-                                                                            <tr>
-                                                                                <td style='text-align: center; padding-bottom:25px'>
-                                                                                    <a href='https://www.reelfruit.com'><img style='height: 50px' src='https://reelfruit.com/images/favicon.png' alt=''></a>
-                                                                                </td>
-                                                                            </tr>
-                                                                        </tbody>
-                                                                    </table>
-                                                                    <center>
-                                                                        <table style='width:100%;max-width:620px;margin:0 auto;background-color:#ffffff;'>
-                                                                            <tbody>
-                                                                                <tr>
-                                                                                    <td style='padding: 30px 30px 15px 30px;'>
-                                                                                        <h2 style='margin: 0;'>Welcome</h2>
-                                                                                    </td>
-                                                                                </tr>
-                                                                                <tr>
-                                                                                    <td style='padding: 0 30px 20px;'>
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    exit("Invalid email address.");
+}
 
-                                                                                        <h4 style='margin-bottom: 10px;''>Thank you for keeping it Reel. Here's your one-time passcode to verify your email address</h4>
+$mail = new PHPMailer(true);
 
-                                                                                        <br>
-                                                                                        <div style='display: flex; justify-content: center; align-items: center; padding:2rem; border: 1px solid #057a4a; color: #212120; font-size: 2rem; letter-spacing: 1rem; font-weight: 600;'>
-                                                                                        
-                                                                                        </div>
-
-                                                                                    </td>
-                                                                                </tr>
-
-                                                                            </tbody>
-                                                                        </table>
-                                                                    </center>
-                                                                    
-                                                                    <br><br>
-                                                    
-                                                                    <center>
-                                                                        <table style='width:100%;max-width:620px;margin-top:0 auto;'>
-                                                                            <tbody>
-                                                                                <tr>
-                                                                                    <td style='text-align: center;'>
-                                                                                        
-                                                                                        <span style='font-size: 13px;'><strong>CONNECT WITH US:</strong></span>
-                                                                                        <br><br>
-
-                                                                                        <a href='www.instagram.com/reelfruit'><img style='height: 20px' src='https://reelfruit.com/images/files/ig.png' alt='IG'></a>
-
-                                                                                        <a href='www.twitter.com/reelfruit'><img style='height: 20px' src='https://reelfruit.com/images/files/tw.png' alt='twitter'></a>
-
-                                                                                        <a href='https://www.linkedin.com/company/reelfruit'><img style='height: 20px' src='https://reelfruit.com/images/files/IN.png' alt='linkedin'></a>
-                                                                        
-                                                                        <a href='www.facebook.com/reelfruit'><img style='height: 20px' src='https://reelfruit.com/images/files/fb2.png' alt='facebook'></a>
-
-                                                                                        
-
-                                                                                    </td>
-                                                                                </tr>
-                                                                            </tbody>
-                                                                        </table>
-                                                                    </center>
-                                                                </td>
-                                                            </tr>
-                                                        </table>
-                                                    </center>";
-                                                        
-                                                        
-
-                                                        
-                                                        $mail = new PHPMailer(true); 
-
-                                                     
-                                                            
-                                                            
-                                                             $mail->isSMTP();
+try {
+    // Server settings
+    $mail->isSMTP();
     $mail->Host       = 'smtp.gmail.com';
     $mail->SMTPAuth   = true;
     $mail->Username   = 'blowaballoon@gmail.com';      // your Gmail
     $mail->Password   = 'tcah pwmw vbgm alhc';        // app password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port       = 587;
-                                                            
-                                                            $mail->IsSendmail();  
-                                                            
-                                                            $mail->From       = "blowaballoon@gmail.com";
-                                                            $mail->FromName   = "ReelFruit";
-                                                            
-                                                            $mail->AddAddress('blowaballoon@gmail.com');
-                                                                $mail->Subject  = "Subject";
-                                                            $mail->WordWrap   = 80; 
-                                                            
-                                                                $mail->MsgHTML($register);
-                                                            $mail->IsHTML(true); 
-                                                            
-                                                            
-                                                            $mail->Send();
-                                                            echo "<h4 class='title-bg success'>Successful!</h4>";
-                                                            echo "<a href='login.php'>
-                                                                    <h4 style='color: #f19d1c; font-weight: 400; text-decoration: underline'>Proceed to Login</h4><br/>
-                                                                </a>";
+
+    // Recipients
+    $mail->setFrom('blowaballoon@gmail.com', 'Website Contact');
+    $mail->addAddress('blowaballoon@gmail.com');        // where messages go
+    $mail->addReplyTo($email, $name);
+
+    // Content
+    $mail->isHTML(false);
+    $mail->Subject = 'New Contact Form Message: '. $subject;
+    $mail->Body    =
+        "Name: {$name}\n" .
+        "Email: {$email}\n\n" .
+        "Message:\n{$message}";
+
+    $mail->send();
+    echo "Message sent successfully.";
+
+} catch (Exception $e) {
+    echo "Mailer Error: {$mail->ErrorInfo}";
+}
                                         
                                            
                                            }
