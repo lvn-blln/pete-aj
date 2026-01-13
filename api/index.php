6198062fc44bfebc39c614ff1dddcfbadd8f1306
lvn-blln <blowaballoon@gmail.com>
2026-01-13 07:07:20 +0100

    remove phpmailer

--- api/index.php
+++ api/index.php
@@ -1,15 +1,3 @@
-<?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require __DIR__ . '/PHPMailer/src/Exception.php';
-require __DIR__ . '/PHPMailer/src/PHPMailer.php';
-// ... other requires if needed
-
-
-
-?>
-
 <!DOCTYPE HTML>
 <html lang="en">
 
